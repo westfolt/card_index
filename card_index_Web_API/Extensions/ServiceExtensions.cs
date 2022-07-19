2131de9f0b0d620aaf3bf664217afd712bd2ecff
Alexandr <westfolt@users.noreply.github.com>
2022-07-19 16:45:29 +0300

    Moved services configuration to extensions methods

---
+++ card_index_Web_API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace card_index_Web_API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        /// <summary>
+        /// Configures Swagger options
+        /// </summary>
+        /// <param name="services">App services collection</param>
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "CardIndex API",
+                    Description = "An ASP.NET Core Web API for managing index of text cards"
+                });
+
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
+                options.AddSecurityDefinition(
+                    "token",
+                    new OpenApiSecurityScheme
+                    {
+                        Type = SecuritySchemeType.Http,
+                        BearerFormat = "JWT",
+                        Scheme = "Bearer",
+                        In = ParameterLocation.Header,
+                        Name = HeaderNames.Authorization
+                    }
+                );
+                options.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "token"
+                                },
+                            },
+                            Array.Empty<string>()
+                        }
+                    }
+                );
+            });
+        }
+        /// <summary>
+        /// Configures CORS options
+        /// </summary>
+        /// <param name="services">App services collection</param>
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CardCorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+        }
+        /// <summary>
+        /// Configures app authentication and JWT settings
+        /// </summary>
+        /// <param name="services">App services collection</param>
+        /// <param name="configuration">App configuration</param>
+        /// <param name="tokenValidHours">Lifespan of token</param>
+        public static void ConfigureAuthentication(this IServiceCollection services, IConfiguration configuration, int tokenValidHours)
+        {
+            services.Configure<DataProtectionTokenProviderOptions>(options =>
+                options.TokenLifespan = TimeSpan.FromHours(tokenValidHours));
+
+            var jwtSettings = configuration.GetSection("JWTSettings");
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(opts =>
+            {
+                opts.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings["validIssuer"],
+                    ValidAudience = jwtSettings["validAudience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                        .GetBytes(jwtSettings.GetSection("securityKey").Value))
+                };
+            });
+        }
+    }
+}
--- card_index_Web_API/Startup.cs
+++ card_index_Web_API/Startup.cs
@@ -1,21 +1,13 @@
 using card_index_BLL.Infrastructure;
 using card_index_BLL.Models.Data;
 using card_index_DAL.Data;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using card_index_Web_API.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.Net.Http.Headers;
-using Microsoft.OpenApi.Models;
-using System;
-using System.IO;
-using System.Reflection;
-using System.Text;
 
 namespace card_index_Web_API
 {
@@ -35,76 +27,9 @@ namespace card_index_Web_API
             var connectionString = Configuration.GetConnectionString("CardIndexConnectionString");
             BllDependencyConfigurator.ConfigureServices(services, connectionString);
             services.AddControllers();
-            services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Version = "v1",
-                    Title = "CardIndex API",
-                    Description = "An ASP.NET Core Web API for managing index of text cards"
-                });
-
-                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-
-                options.AddSecurityDefinition(
-                    "token",
-                    new OpenApiSecurityScheme
-                    {
-                        Type = SecuritySchemeType.Http,
-                        BearerFormat = "JWT",
-                        Scheme = "Bearer",
-                        In = ParameterLocation.Header,
-                        Name = HeaderNames.Authorization
-                    }
-                );
-                options.AddSecurityRequirement(
-                    new OpenApiSecurityRequirement
-                    {
-                        {
-                            new OpenApiSecurityScheme
-                            {
-                                Reference = new OpenApiReference
-                                {
-                                    Type = ReferenceType.SecurityScheme,
-                                    Id = "token"
-                                },
-                            },
-                            Array.Empty<string>()
-                        }
-                    }
-                );
-            });
-            services.AddCors(options =>
-            {
-                options.AddPolicy("CardCorsPolicy", builder =>
-                    builder.AllowAnyOrigin()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader());
-            });
-
-            services.Configure<DataProtectionTokenProviderOptions>(options =>
-                options.TokenLifespan = TimeSpan.FromHours(2));
-
-            var jwtSettings = Configuration.GetSection("JWTSettings");
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(opts =>
-            {
-                opts.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = jwtSettings["validIssuer"],
-                    ValidAudience = jwtSettings["validAudience"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
-                        .GetBytes(jwtSettings.GetSection("securityKey").Value))
-                };
-            });
+            services.ConfigureSwagger();
+            services.ConfigureCors();
+            services.ConfigureAuthentication(Configuration, 2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -123,14 +48,10 @@ namespace card_index_Web_API
             });
 
             app.UseHttpsRedirection();
-
             app.UseCors("CardCorsPolicy");
-
             app.UseRouting();
-
             app.UseAuthentication();
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
